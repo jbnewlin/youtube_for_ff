db5140b6a04e6bd645a1d407e791369e3c69f1c5
Joel Van Auken <posiden104@gmail.com>
2017-11-16 01:02:46 -0500

    console print on extension load

--- youtube_for_ff.js
+++ youtube_for_ff.js
@@ -1,1 +1,10 @@
 // javascript for writing youtube stuff
+
+
+console.log("Youtube for FireFox is enabled.");
+
+
+console.log(document.getElementById("ytd-page-manager"));
+//document.getElementById("ytd-page-manager").style.backgroundColor = "#696969";
+
+console.log(document.getElementById("ytd-app").style);
